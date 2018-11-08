089b81d83154bad63229a8c73655f7c70b2e0b86
Toni Rikkola <toni@rikkola.net>
2018-11-08 22:47:03 +0200

    dead code

--- drools-verifier/drools-verifier-core/src/main/java/org/drools/verifier/core/maps/ArrayListSubMapProvider.java
+++ drools-verifier/drools-verifier-core/src/main/java/org/drools/verifier/core/maps/ArrayListSubMapProvider.java
@@ -18,9 +18,7 @@ package org.drools.verifier.core.maps;
 
 import java.util.ArrayList;
 
-//import org.jboss.errai.common.client.api.annotations.Portable;
 
-//@Portable
 public class ArrayListSubMapProvider<Value>
         implements NewSubMapProvider<Value, ArrayList<Value>> {
 
