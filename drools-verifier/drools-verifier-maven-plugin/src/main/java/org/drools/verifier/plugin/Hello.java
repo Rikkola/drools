371ab63579af20fc7753a3339e76559a97e47976
rikkola <toni@rikkola.net>
2020-04-10 18:30:12 +0300

    MAVEN PLUGIN AND MOVE

---
+++ drools-verifier/drools-verifier-maven-plugin/src/main/java/org/drools/verifier/plugin/Hello.java
@@ -0,0 +1,13 @@
+package org.drools.verifier.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "sayhi")
+public class Hello extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException {
+        getLog().info("Hello, world.");
+    }
+}
