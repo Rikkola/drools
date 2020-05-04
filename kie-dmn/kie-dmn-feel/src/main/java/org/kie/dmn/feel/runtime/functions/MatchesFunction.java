4ed7320213fda98145fee36fb357e36b2c31cf3c
rikkola <toni@rikkola.net>
2020-05-04 08:58:09 +0300

    hello

--- kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/MatchesFunction.java
+++ kie-dmn/kie-dmn-feel/src/main/java/org/kie/dmn/feel/runtime/functions/MatchesFunction.java
@@ -18,11 +18,13 @@ package org.kie.dmn.feel.runtime.functions;
 
 import org.kie.dmn.api.feel.runtime.events.FEELEvent.Severity;
 import org.kie.dmn.feel.runtime.events.InvalidParametersEvent;
+import org.kie.dmn.model.api.GwtIncompatible;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+@GwtIncompatible
 public class MatchesFunction
         extends BaseFEELFunction {
 
